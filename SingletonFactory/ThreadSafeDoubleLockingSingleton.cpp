bf66769eff7422491a11c47b6238c569faaf269e
Vishal Kumar <vishalkumar150206@gmail.com>
2026-07-26 16:32:28 +0530

    fix the error

--- SingletonFactory/ThreadSafeDoubleLockingSingleton.cpp
+++ SingletonFactory/ThreadSafeDoubleLockingSingleton.cpp
@@ -1,37 +1,27 @@
-#include<iostream>
-#include <mutex>
+#include <iostream>
 
 using namespace std;
 
 class Singleton {
 private:
-    static Singleton* instance;
-    static mutex mtx;
-
-    Singleton() { 
-        cout << "Singleton Constructor Called!" << endl; 
+    Singleton() {
+        cout << "Singleton Constructor Called!" << endl;
     }
- 
+
+    // Prevent copying
+    Singleton(const Singleton&) = delete;
+    Singleton& operator=(const Singleton&) = delete;
+
 public:
-    // Double check locking..
-    static Singleton* getInstance() {
-        if (instance == nullptr) {  // First check (no locking)
-            lock_guard<mutex> lock(mtx);  // Lock only if needed
-            if (instance == nullptr) {  // Second check (after acquiring lock)
-                instance = new Singleton();
-            }
-        }
+    static Singleton& getInstance() {
+        static Singleton instance;   // Thread-safe in C++11+
         return instance;
     }
 };
- 
-// Initialize static members
-Singleton* Singleton::instance = nullptr;
-mutex Singleton::mtx;
 
 int main() {
-    Singleton* s1 = Singleton::getInstance();
-    Singleton* s2 = Singleton::getInstance();
+    Singleton& s1 = Singleton::getInstance();
+    Singleton& s2 = Singleton::getInstance();
 
-    cout << (s1 == s2) << endl;
+    cout << (&s1 == &s2) << endl;
 }
